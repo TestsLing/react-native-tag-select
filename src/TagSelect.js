2a94071e1dd338dd75d4f593ae2dd8ff019ec8db
yudezhi <15827617798@163.com>
2019-06-17 13:16:49 +0800

    新增取消所有选中方法

--- src/TagSelect.js
+++ src/TagSelect.js
@@ -83,6 +83,13 @@ class TagSelect extends React.Component {
   }
 
   /**
+   * clear all selected
+   */
+  get clearSelected () {
+    this.setState({value:{}})
+  }
+
+  /**
    * @description Callback after select an item
    * @param {Object} item
    * @return {Void}
